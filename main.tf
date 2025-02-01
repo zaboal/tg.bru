eecf1fbc899fbe92773e78879aa7bfcc24c6473c
2happy <biv888iii888@yandex.ru>
2025-02-01 17:01:55 +0300

    ready terraform

---
+++ main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.136.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.0.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token     = var.yandex_token
+  cloud_id  = var.yandex_cloud_id
+  folder_id = var.yandex_folder_id
+  zone      = "ru-central1-a"
+}
+
+resource "random_id" "user_hash" {
+  byte_length = 16
+}
+
+data "archive_file" "function_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/src/functional"
+  output_path = "${path.module}/archive.zip"
+}
+
+resource "yandex_function" "my_function" {
+  name              = "terraform-function"
+  description       = "For tg"
+  user_hash         = random_id.user_hash.hex
+  runtime           = "php82"
+  entrypoint        = "index.handler"
+  memory            = 128
+  execution_timeout = 10
+
+  content {
+    zip_filename = data.archive_file.function_zip.output_path
+  }
+
+  environment = {
+    API_KEY        = var.api_key
+    ADMINS         = var.admins
+    ACCOUNT        = var.account
+    SECRET         = var.secret
+    APP_ID         = var.app_id
+    TINYBIRD_TOKEN = var.tinybird_token
+  }
+}
+
+resource "null_resource" "trigger_webhook" {
+  depends_on = [yandex_function.my_function]
+
+  provisioner "local-exec" {
+    command = "curl -X POST \"https://api.telegram.org/bot${var.api_key}/setWebhook?url=https://functions.yandexcloud.net/${yandex_function.my_function.id}\""
+  }
+}
+
+output "function_https_url" {
+  value = yandex_function.my_function.id
+  }
+
---
+++ variables.tf
@@ -0,0 +1,60 @@
+variable "service_account" {
+	description = "service-account"
+	type = string
+	sensitive = true
+}
+
+variable "yandex_token" {
+  description = "Yandex Cloud OAuth token"
+  type        = string
+  sensitive   = true
+}
+
+variable "yandex_cloud_id" {
+  description = "Yandex Cloud ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "yandex_folder_id" {
+  description = "Yandex Cloud Folder ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "api_key" {
+  description = "API key для Telegram"
+  type        = string
+  sensitive = true
+}
+
+variable "admins" {
+  description = "Список администраторов (в виде строки, разделённой запятыми)"
+  type        = string
+}
+
+variable "account" {
+  description = "Account для business_ru"
+  type        = string
+  sensitive = true
+}
+
+variable "secret" {
+  description = "Secret для business_ru"
+  type        = string
+  sensitive = true
+}
+
+variable "app_id" {
+  description = "APP_ID для business_ru"
+  type        = string
+  sensitive = true
+}
+
+variable "tinybird_token" {
+  description = "Token для tinybird"
+  type        = string
+  sensitive = true
+}
+
+
