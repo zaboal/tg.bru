187ed9ba027343b983d62a8f8c5802e404f10ec0
2happy <biv888iii888@yandex.ru>
2025-02-05 00:28:00 +0300

    The end

--- src/bru/index.php
+++ src/bru/index.php
@@ -1,13 +1,50 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
-/* require_once __DIR__ . '/log.php'; */
+function sendMessage($chat, $text)
+{
+	$data = [
+		'chat_id' => $chat,
+		'text' => $text
+	];
+	$api = $_ENV['API_KEY'];
+	$url = "https://api.telegram.org/bot$api/sendMessage";
+	$ch = curl_init();
 
-Dotenv\Dotenv::createImmutable(__DIR__)->load();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-// интеграция https://telegram.org
-use bru\api\Client;
-use bru\api\NoCache;
+	curl_exec($ch);
+	curl_close($ch);
+}
+
+function getChat($number)
+{
+	$url = 'https://api.us-east.aws.tinybird.co/v0/pipes/untitled_pipe_7865.json';
+	$params = ['num' => $number];
+	$token = $_ENV['TOKEN'];
+	$ch = curl_init();
+	curl_setopt_array($ch, [
+		CURLOPT_URL => $url . '?' . http_build_query($params),
+		CURLOPT_HTTPHEADER => [
+			'Authorization: Bearer ' . $token,
+			'Accept-Encoding: gzip'
+		],
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_TIMEOUT => 30
+	]);
+
+	$response = curl_exec($ch);
+
+	$response = json_decode(gzdecode($response));
+	curl_close($ch);
+	if (! $response->data[0]->tg) {
+		return false;
+	}
+	return (int)$response->data[0]->tg;
+}
 
 function handler($event, $context)
 {
@@ -15,20 +52,14 @@ function handler($event, $context)
 	if (! $event['body']) {
 		throw new Exception("No body in mesage");
 	}
+
 	$текст = base64_decode($event['body'], true);
 	parse_str($текст, $params);
 	$changes = json_decode($params['changes'], true);
+	$data = json_decode($params['data']);
 	$new = $changes[1]['data']['bonus_sum'];
 	$delta = $new - $changes[0]['data']['bonus_sum'];
 	$text = "Благодарим за покупку!\nНачислено " . $new . " баллов, теперь у Вас " . $delta . " баллов";
-	echo $text;
-
-	$api = $_ENV['API_KEY'];
-	$url = "https://api.telegram.org/bot$api/sendMessage";
-	echo $url;
-	
-	/* echo $event['body']; */
-	/* log_message($event['body']); */
+	$id = getChat($data->num);
+	sendMessage($id, $text);
 }
-
-handler(['body' => 'YXBwX2lkPTk2ODQyNiZtb2RlbD1kaXNjb3VudGNhcmRzJmFjdGlvbj11cGQmY2hhbmdlcz0lN0IlMjIwJTIyJTNBJTdCJTIyZW1wbG95ZWVfcmVmJTIyJTNBbnVsbCUyQyUyMmFjdGlvbiUyMiUzQSUyMnVwZCUyMiUyQyUyMmRhdGElMjIlM0ElN0IlMjJpZCUyMiUzQW51bGwlMkMlMjJudW0lMjIlM0FudWxsJTJDJTIyZGlzY291bnRfY2FyZF90eXBlX2lkJTIyJTNBbnVsbCUyQyUyMnBhcnRuZXJfaWQlMjIlM0FudWxsJTJDJTIyZGF0ZV9iZWdpbiUyMiUzQW51bGwlMkMlMjJkYXRlX2VuZCUyMiUzQW51bGwlMkMlMjJjdXJyZW50X2Rpc2NvdW50X3ZhbHVlJTIyJTNBbnVsbCUyQyUyMnN1bV9jdXJyZW50JTIyJTNBbnVsbCUyQyUyMnN1bV9maXQlMjIlM0FudWxsJTJDJTIyYm9udXNfc3VtJTIyJTNBMTAzNCUyQyUyMnVwZGF0ZWQlMjIlM0FudWxsJTJDJTIyZGVsZXRlZCUyMiUzQW51bGwlN0QlN0QlMkMlMjIxJTIyJTNBJTdCJTIyZW1wbG95ZWVfcmVmJTIyJTNBbnVsbCUyQyUyMmFjdGlvbiUyMiUzQSUyMnVwZCUyMiUyQyUyMmRhdGElMjIlM0ElN0IlMjJpZCUyMiUzQW51bGwlMkMlMjJudW0lMjIlM0FudWxsJTJDJTIyZGlzY291bnRfY2FyZF90eXBlX2lkJTIyJTNBbnVsbCUyQyUyMnBhcnRuZXJfaWQlMjIlM0FudWxsJTJDJTIyZGF0ZV9iZWdpbiUyMiUzQW51bGwlMkMlMjJkYXRlX2VuZCUyMiUzQW51bGwlMkMlMjJjdXJyZW50X2Rpc2NvdW50X3ZhbHVlJTIyJTNBbnVsbCUyQyUyMnN1bV9jdXJyZW50JTIyJTNBbnVsbCUyQyUyMnN1bV9maXQlMjIlM0FudWxsJTJDJTIyYm9udXNfc3VtJTIyJTNBMTEzNCUyQyUyMnVwZGF0ZWQlMjIlM0FudWxsJTJDJTIyZGVsZXRlZCUyMiUzQW51bGwlN0QlN0QlN0QmZGF0YT0lN0IlMjJpZCUyMiUzQTE3MTg2JTJDJTIybnVtJTIyJTNBJTIyOTkyMTk1NTEyNyUyMiUyQyUyMmRpc2NvdW50X2NhcmRfdHlwZV9pZCUyMiUzQTglMkMlMjJwYXJ0bmVyX2lkJTIyJTNBbnVsbCUyQyUyMmRhdGVfYmVnaW4lMjIlM0FudWxsJTJDJTIyZGF0ZV9lbmQlMjIlM0FudWxsJTJDJTIyY3VycmVudF9kaXNjb3VudF92YWx1ZSUyMiUzQSUyMjUlMjIlMkMlMjJzdW1fY3VycmVudCUyMiUzQSUyMjAlMjIlMkMlMjJzdW1fZml0JTIyJTNBJTIyMCUyMiUyQyUyMmJvbnVzX3N1bSUyMiUzQSUyMjEyMzQlMjIlMkMlMjJ1cGRhdGVkJTIyJTNBJTIyMDQuMDIuMjAyNSswMCUzQTM1JTNBNDguNTI5NDcyJTIyJTJDJTIyZGVsZXRlZCUyMiUzQWZhbHNlJTdEJmFwcF9wc3c9Y2E1YzYwNjBlNjE1YTJhM2NkZDNlMmMyNzEyZGMwZDM='], 1);
