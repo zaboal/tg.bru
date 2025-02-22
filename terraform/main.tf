b99e3eb42cc46672da371814e0ad6decfac9f7d8
Bogdan Zažigin <me@zba.su>
2025-02-22 15:42:25 +0000

    Переместить версии и настройки провайдеров в

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = ">= 0.136.0"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = ">= 2.0.0"
-    }
-    curl = {
-      source  = "marcofranssen/curl"
-      version = "0.7.0"
-    }
-  }
-  required_version = ">= 0.13"
-}
-
-provider "yandex" {
-  token     = var.yandex_token
-  cloud_id  = var.yandex_cloud_id
-  folder_id = var.yandex_folder_id
-  zone      = "ru-central1-a"
-}
-
 resource "random_id" "user_hash" {
   byte_length = 16
 }
