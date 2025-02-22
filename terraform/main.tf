243c51aeddd4b9c1990045f77c3fcac07138c107
Bogdan Zažigin <me@zba.su>
2025-02-22 14:24:28 +0000

    Заране исправить ссылу на эндпонит для установки данных для вебхука

--- terraform/main.tf
+++ terraform/main.tf
@@ -109,6 +109,6 @@ data "curl_request" "businessru_webhook" {
 
 data "curl_request" "businessru_webhook_discountcards" {
   depends_on  = [yandex_function.businessru]
-  uri         = "https://${var.businessru_account_id}.business.ru/api/rest/webhookurl.json?app_id=${var.businessru_app_id}&webhook_model_id=42&app_psw=${md5(join("", [jsondecode(data.curl_request.businessru_token.response_body).token, var.businessru_app_secret, "app_id=", var.businessru_app_id, "&webhook_model_id=42"]))}"
+  uri         = "https://${var.businessru_account_id}.business.ru/api/rest/webhooks.json?app_id=${var.businessru_app_id}&webhook_model_id=42&app_psw=${md5(join("", [jsondecode(data.curl_request.businessru_token.response_body).token, var.businessru_app_secret, "app_id=", var.businessru_app_id, "&webhook_model_id=42"]))}"
   http_method = "POST"
 }
