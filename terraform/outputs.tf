84b93a03a3b5067d98f4d715edf71d7be3f592c5
Bogdan Zažigin <me@zba.su>
2025-02-22 12:16:12 +0000

    Удалить не нужный вывод и отформатировать

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,26 +1,7 @@
-output "businessru_function_url" {
-	value = "https://functions.yandexcloud.net/${yandex_function.businessru.id}"
-	description = <<-EOT
-	Ссылка на функцию для Бизнес.Ру в Yandex Cloud
-	Для ручной установки в качестве вебхука для интеграции
-	EOT
-}
-
-output "telegram_function_url" {
-	value = "https://functions.yandexcloud.net/${yandex_function.telegram.id}"
-	description = <<-EOT
-	Ссылка на функцию для Telegram в Yandex Cloud
-	EOT
-}
-
 output "telegram_url" {
-  value = "https://t.me/${nonsensitive(jsondecode(data.curl_request.telegram_getme.response_body).result.username)}"
-	description = <<-EOT
-	Ссылка на бота в Telegram
-	https://core.telegram.org/api/links#public-username-links
-	EOT 
-}
-
-output "businessru_app_psw_webhook" {
-    value = nonsensitive(join("", [jsondecode(data.curl_request.businessru_token.response_body).token , var.businessru_app_secret, "app_id=", var.businessru_app_id, "&url=https://functions.yandexcloud.net/", yandex_function.businessru.id]))
+  value       = "https://t.me/${nonsensitive(jsondecode(data.curl_request.telegram_getme.response_body).result.username)}"
+  description = <<-EOT
+  Ссылка на бота в Telegram
+  https://core.telegram.org/api/links#public-username-links
+  EOT 
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,90 +1,90 @@
-variable "tinybird_token" {  
-  type				= string
-  sensitive		= true
-  nullable		= false
-	description = <<-EOT
-	Токен для Tinybird, базы данных, с полным доступом
-	https://tinybird.co/docs/get-started/administration/auth-tokens#create-a-token
-	EOT
+variable "tinybird_token" {
+  type        = string
+  sensitive   = true
+  nullable    = false
+  description = <<-EOT
+  Токен для Tinybird, базы данных, с полным доступом
+  https://tinybird.co/docs/get-started/administration/auth-tokens#create-a-token
+  EOT
 }
 
 variable "yandex_cloud_id" {
   type        = string
   sensitive   = false
-	nullable		= false
-	description	= <<-EOT
-	Идентификатор облака Yandex Cloud, где будет находится каталог с ботом
-	https://yandex.cloud/docs/resource-manager/operations/cloud/get-id
-	EOT
+  nullable    = false
+  description = <<-EOT
+  Идентификатор облака Yandex Cloud, где будет находится каталог с ботом
+  https://yandex.cloud/docs/resource-manager/operations/cloud/get-id
+  EOT
 }
 
 variable "yandex_folder_id" {
   type        = string
   sensitive   = false
-	nullable		= false
-	description	= <<-EOT
-	Идентификатор каталога Yandex Cloud, где будет развёрнут бот
-	https://yandex.cloud/docs/resource-manager/operations/folder/get-id
-	EOT
+  nullable    = false
+  description = <<-EOT
+  Идентификатор каталога Yandex Cloud, где будет развёрнут бот
+  https://yandex.cloud/docs/resource-manager/operations/folder/get-id
+  EOT
 }
 
 variable "yandex_token" {
   type        = string
   sensitive   = true
-	nullable		= false
-	description	= <<-EOT
-	Токен OAuth для Yandex Cloud
-	https://yandex.cloud/docs/iam/concepts/authorization/oauth-token
-	EOT
+  nullable    = false
+  description = <<-EOT
+  Токен OAuth для Yandex Cloud
+  https://yandex.cloud/docs/iam/concepts/authorization/oauth-token
+  EOT
 }
 
 variable "telegram_admins_ids" {
   type        = list(number)
-	sensitive 	= false
-	nullable		= false
-	default 		= ["987595197"]
-	description	= <<-EOT
-	ID администраторов бота в Telegram
-	Свой и чужой ID можно узнать через https://t.me/myidbot
-	EOT
+  sensitive   = false
+  nullable    = false
+  default     = ["987595197"]
+  description = <<-EOT
+  ID администраторов бота в Telegram
+  Свой и чужой ID можно узнать через https://t.me/myidbot
+  EOT
 }
 
 variable "telegram_token" {
   type        = string
-  sensitive		= true
-	nullable		= false
-	description	= <<-EOT
-	Токен бота в Telegram
-	https://core.telegram.org/bots/tutorial#obtain-your-bot-token
-	EOT
+  sensitive   = true
+  nullable    = false
+  description = <<-EOT
+  Токен бота в Telegram
+  https://core.telegram.org/bots/tutorial#obtain-your-bot-token
+  EOT
 }
 
 variable "businessru_account_id" {
   type        = string
-  sensitive 	= false
-  nullable		= false
-	description	= <<-EOT
-	Идентификатор склада из URL в Бизнес.Ру
-	Находится на его странице: https://[ЗДЕСЬ].beta-class365.ru/
-	EOT
+  sensitive   = false
+  nullable    = false
+  description = <<-EOT
+  Идентификатор склада из URL в Бизнес.Ру
+  Находится на его странице: https://[ЗДЕСЬ].beta-class365.ru/
+  EOT
 }
 
 variable "businessru_app_id" {
-	type				= number
-  sensitive		= false
-  nullable		= false
-	description	= <<-EOT
-	ID интеграции в Бизнес.Ру
-	Получается при создании "Интеграции по API" в магазине приложений
-	EOT
+  type        = number
+  sensitive   = false
+  nullable    = false
+  description = <<-EOT
+  ID интеграции в Бизнес.Ру
+  Получается при создании "Интеграции по API" в магазине приложений
+  EOT
 }
 
 variable "businessru_app_secret" {
-  type				= string
-  sensitive 	= true
-	nullable 		= false 
-	description	= <<-EOT
-	Секретный ключ интеграции в Бизнес.Ру
-	Получается при создании "Интеграции по API" в магазине приложений
-	EOT
+  type        = string
+  sensitive   = true
+  nullable    = false
+  description = <<-EOT
+  Секретный ключ интеграции в Бизнес.Ру
+  Получается при создании "Интеграции по API" в магазине приложений
+  EOT
 }
